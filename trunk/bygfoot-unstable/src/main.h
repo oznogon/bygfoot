8bff46e36f45f33735a8f728f527f546dfa3aee0
gyboth <gyboth@47133fec-3b27-0410-a0ec-912ca9ec1db5>
2004-08-29 16:43:20 +0000

    Change .c/.h files structure: remove old files.

--- trunk/bygfoot-unstable/src/main.h
+++
@@ -1,8 +0,0 @@
-/*************************
- * functions from misc.c *
- *************************/
-
-/* check for the files with team and player names
-   and help file */
-void check_files(void);
-
