1809d0175001fe349606c5e6aeef164b4e81465d
nobody <nobody@47133fec-3b27-0410-a0ec-912ca9ec1db5>
2005-02-19 11:57:45 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_8_1'.

---
+++ tags/release_1_8_1/bygfoot-unstable/src/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "bygfoot.h"
+#include "variables.h"
+
+/* set some variables that are used but not saved
+   in the game */
+void
+set_variables(void);
+
+void
+bygfoot_init(gint argc, gchar *argv[]);
+
+gint
+main (gint argc, gchar *argv[]);
+
+#endif
