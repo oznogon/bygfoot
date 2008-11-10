13ba5a4320ba8fc97e4f78615bf7981df1a76def
gunnar_g <gunnar_g@47133fec-3b27-0410-a0ec-912ca9ec1db5>
2008-11-10 20:33:50 +0000

    Create release 2.3.0 branch

---
+++ bygfoot/branches/release_2_3_0/src/league_table_callbacks.h
@@ -0,0 +1,12 @@
+#include <gtk/gtk.h>
+
+
+
+gboolean
+on_window_league_table_delete_event    (GtkWidget       *widget,
+                                        GdkEvent        *event,
+                                        gpointer         user_data);
+
+void
+on_lt_b_ok_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
